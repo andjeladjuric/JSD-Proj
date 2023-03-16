3ea1c5be3ce2dd89f6ebe1eebb694f5183b830d1
ivanagavric <gavric.ra160.2017@uns.ac.rs>
2023-03-16 23:10:37 +0100

    fix: fixed the error occurred when listing tasks/tasklists

--- calendarlang/tasks.py
+++ calendarlang/tasks.py
@@ -2,32 +2,35 @@ from datetime import datetime
 
 class Tasks():
     def __find_list_by_title(self, tasklists, title):
-        for list in tasklists:
-            if list["title"] == title: return list
+        if tasklists != None:
+            for list in tasklists:
+                if list["title"] == title: return list
 
     def query_tasks_by_tasklist_and_status(self, calendar_model, tasks_service):
         found_tasks = []
+        try:
+            for rule in calendar_model.findTasks:
+                title = rule.tasklist
+                status = rule.status
 
-        for rule in calendar_model.findTasks:
-            title = rule.tasklist
-            status = rule.status
+                tasklists_result = tasks_service.tasklists().list().execute()
+                tasklists = tasklists_result.get('items', [])
+                completed_max = datetime.utcnow().isoformat() + 'Z' if status == 'completed' else None
 
-            tasklists_result = tasks_service.tasklists().list().execute()
-            tasklists = tasklists_result.get('items', [])
-            completed_max = datetime.utcnow().isoformat() + 'Z' if status == 'completed' else None
+                tasklist = self.__find_list_by_title(tasklists, title)
+                tasks_result = tasks_service.tasks().list(
+                    tasklist=tasklist['id'],
+                    showCompleted=True,
+                    completedMax=completed_max
+                ).execute()
+                tasks = tasks_result.get('items', [])
+                found_tasks.extend(tasks)
 
-            tasklist = self.__find_list_by_title(tasklists, title)
-            tasks_result = tasks_service.tasks().list(
-                tasklist=tasklist['id'],
-                showCompleted=True,
-                completedMax=completed_max
-            ).execute()
-            tasks = tasks_result.get('items', [])
-            found_tasks.extend(tasks)
-        
-        for task in tasks:
-            print("\n{} \n\tstatus: {} \n\tdue: {}".format(
-                task['title'], task['status'], task['due']))
+            for task in tasks:
+                print("\n{} \n\tstatus: {} \n\tdue: {}".format(
+                    task['title'], task['status'], task['due']))
+        except:
+            print(f'Tasklist or task is none!')   
 
     def tasklist_data(self, tasklist):
         try:
@@ -38,15 +41,13 @@ class Tasks():
         except:
             return None
 
-
     def create_new_tasklist(self, calendar_model, tasks_service):
         for tasklist in calendar_model.tasklists:
             tasklist_data = self.tasklist_data(tasklist)
             tasks_service.tasklists().insert(body=tasklist_data).execute()
-        
-        print(f"Tasklist is created!")
 
-            
+            print(f"Tasklist with name {tasklist.title} is created!")
+
     def delete_tasklist(self, tasks_service, tasklist_name):
         tasklists_result = tasks_service.tasklists().list().execute()
         tasklists = tasklists_result.get('items', [])
@@ -74,7 +75,6 @@ class Tasks():
         start_time = datetime(year, month, day, hour, minute, tzinfo=datetime.now(pytz.utc).astimezone().tzinfo)
         return (start_time)
 
-
     def task_data(self, task):
         try:
             task_data = {
@@ -89,8 +89,10 @@ class Tasks():
             return None
 
     def insert_task(self, tasks_service, calendar_model):
-        for task in calendar_model.tasks:
-            task_data = self.task_data(task)
-            if (task_data != None):
-                tasks_service.tasks().insert(tasklist='@default',body=task_data).execute()
-
+        try:
+            for task in calendar_model.tasks:
+                task_data = self.task_data(task)
+                if (task_data != None):
+                    tasks_service.tasks().insert(tasklist='@default',body=task_data).execute()
+        except:
+            print(f'An error found when creating task!')
